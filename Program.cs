67c4c96ec1e0e5592de34c5487ad4625a74be735
Conor Getty <conorgetty@gmail.com>
2023-05-22 15:32:44 -0500

    chat GPT changed my program. Not great, but a decent references for encapsulation

--- Program.cs
+++ Program.cs
@@ -6,73 +6,93 @@ namespace GuessingGame
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Guessing Game where you will attempt to guess my secret number betwen 1 and 100");
-            Console.WriteLine("Choose a difficulty by typing one of the following: easy (8 guesses), medium (6), hard (4), cheater (infinite)");
+            RunGame();
+        }
+
+        static void RunGame()
+        {
+            Console.WriteLine("Welcome to the Guessing Game! I have a secret number between 1 and 100.");
+            Console.WriteLine("Choose a difficulty: easy (8 guesses), medium (6), hard (4), or cheater (infinite guesses).");
             string difficulty = Console.ReadLine();
-            int chances = 0;
-            while (chances == 0)
+
+            int chances = GetChances(difficulty);
+            if (chances == 0)
             {
-                if (difficulty == "easy")
-                {
-                    chances = 8;
-                }
-                else if (difficulty == "medium")
-                {
-                    chances = 6;
-                }
-                else if (difficulty == "hard")
-                {
-                    chances = 4;
-                }
-                else if (difficulty == "cheater")
-                {
-                    chances = 1;
-                }
-                else
-                {
-                    Console.WriteLine("Invalid input. Please choose a difficulty by typing easy, medium, hard, or cheater:");
-                    difficulty = Console.ReadLine();
-                }
+                Console.WriteLine("Invalid input. Please choose a difficulty by typing easy, medium, hard, or cheater.");
+                RunGame();
+                return;
             }
-            int runningChances = chances;
+
+            PlayGame(chances);
+
+            Console.WriteLine("Do you want to play again? (yes/no)");
+            string playAgainInput = Console.ReadLine();
+            playAgainInput = playAgainInput.ToLower();
+            while (playAgainInput != "yes" && playAgainInput != "no")
+            {
+                Console.WriteLine("Invalid input. Please enter 'yes' or 'no'.");
+                playAgainInput = Console.ReadLine();
+                playAgainInput = playAgainInput.ToLower();
+            }
+
+            if (playAgainInput == "yes")
+            {
+                RunGame();
+            }
+        }
+
+        static int GetChances(string difficulty)
+        {
+            switch (difficulty)
+            {
+                case "easy":
+                    return 8;
+                case "medium":
+                    return 6;
+                case "hard":
+                    return 4;
+                case "cheater":
+                    return int.MaxValue;
+                default:
+                    return 0;
+            }
+        }
+
+        static void PlayGame(int chances)
+        {
             Random random = new Random();
             int randomNumber = random.Next(1, 101);
-            while (runningChances > 0)
+
+            Console.WriteLine("I've picked my secret number. Start guessing!");
+
+            for (int i = 0; i < chances; i++)
             {
-                if (runningChances == chances)
-                {
-                    Console.WriteLine($"Okay. I'll give you {chances} guess(es)");
-                }
-                else
-                {
-                    Console.WriteLine($"You have {runningChances} guess(es) left. What is your next guess?");
-                }
-                if (difficulty != "cheater")
-                {
-                    runningChances--;
-                }
+                Console.WriteLine($"You have {chances - i} guess(es) left. What is your next guess?");
                 string guess = Console.ReadLine();
+
                 if (!int.TryParse(guess, out int guessNumber) || guessNumber < 1 || guessNumber > 100)
                 {
                     Console.WriteLine("Invalid guess. Please enter a number between 1 and 100.");
-                    runningChances++;
+                    i--;
                     continue;
                 }
 
                 if (guessNumber == randomNumber)
                 {
                     Console.WriteLine("You guessed it!");
-                    break;
+                    return;
                 }
                 else if (guessNumber > randomNumber)
                 {
-                    Console.WriteLine("You failed... Your guess was too high!");
+                    Console.WriteLine("Your guess was too high!");
                 }
                 else
                 {
-                    Console.WriteLine("You failed... Your guess was too low!");
+                    Console.WriteLine("Your guess was too low!");
                 }
             }
+
+            Console.WriteLine($"Sorry, you ran out of guesses. The secret number was {randomNumber}.");
         }
     }
 }
