33ae1c0d3c0b405db8e69969801d76f60a60a050
Conor Getty <conorgetty@gmail.com>
2023-05-22 11:13:50 -0500

    completed phase 2

--- Program.cs
+++ Program.cs
@@ -6,9 +6,17 @@ namespace GuessingGame
     {
         static void Main(string[] args)
         {
+            string secretNumber = "12";
             Console.WriteLine("Would you like to guess my secret number?");
             string guess = Console.ReadLine();
-            Console.WriteLine(guess);
+            if (guess == secretNumber)
+            {
+                Console.WriteLine("You guessed it!");
+            }
+            else
+            {
+                Console.WriteLine("You failed...");
+            }
         }
     }
 }
